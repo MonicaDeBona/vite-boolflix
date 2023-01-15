d096d88c21fe8ed5f33a43b59e49716b3f22d58b
MonicaDeBona <debona.monica@outlook.it>
2023-01-15 16:16:59 +0100

    Add header and main in App template and populated styles

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import { store } from './store.js';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
-export default{
+export default {
     components: {
         AppHeader,
         AppMain,
     },
-    
+
     data() {
         return {
             store,
@@ -19,7 +19,12 @@ export default{
 </script>
 
 <template>
-
+    <header>
+        <AppHeader />
+    </header>
+    <main>
+        <AppMain />
+    </main>
 </template>
 
 <style lang="scss">
